4d16a23499060bc6243014359e419703d63de68d
Venkat Venky <maddivenkatateja.23.csm@anits.edu.in>
2026-04-10 18:43:31 +0530

    Fix nested repo issue

---
+++ scaler/build/lib/my_env/__init__.py
@@ -0,0 +1,2 @@
+from .env import MyEnv
+from .models import Action, Observation
---
+++ scaler/build/lib/my_env/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from .env import MyEnv
+from .models import Action
+
+app = FastAPI()
+
+env = MyEnv()
+
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return {"observation": obs.dict(), "done": False}
+
+@app.post("/step")
+def step(action: Action):
+    result = env.step(action)
+    return {
+        "observation": result.observation.dict(),
+        "reward": result.reward,
+        "done": result.done,
+        "info": result.info
+    }
+
+@app.get("/state")
+def state():
+    return env.state()
